3bcbb190e168d5808208d56f6d07d4af56a0b0f8
HUHANK <982857141@qq.com>
2021-07-30 22:59:50 +0800

    添加文件

---
+++ Finance/Test/test1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/12/12 19:16
+# @Author : HuYouLiang
+# @File : test1.py
+# @Purpose :
+
+name = 'ada lovelace'
+print(name.title())
+
+val1=1_000_000_000
+print(val1)
+MAX_INT = 123
+MAX_INT=1
+print(MAX_INT)
