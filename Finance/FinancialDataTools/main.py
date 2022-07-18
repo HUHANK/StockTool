9ad39828edbacf596cc8a2b18c9bcef9abc7becd
youliang.hu@meetfuture.cn <youliang.hu@meetfuture.cn>
2022-07-18 17:43:26 +0800

    fixed

--- Finance/FinancialDataTools/main.py
+++ Finance/FinancialDataTools/main.py
@@ -5,10 +5,16 @@
 # @File     : main.py
 # @Project  : Finance
 
+import os
 import sys
+
+# 把当前文件所在文件夹的父文件夹路径加入到PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from PySide2.QtWidgets import QApplication
-from FinancialDataTools.ui.MainWindow import MainWindow
+
 from FinancialDataTools.shared.AppConfig import CONFIG
+from FinancialDataTools.ui.MainWindow import MainWindow
 
 
 def SystemInit() -> bool:
